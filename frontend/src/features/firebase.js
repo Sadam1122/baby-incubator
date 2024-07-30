31288fb4a54da736f6bc095a97b2ab257067452d
Sadam1122 <90631818+Sadam1122@users.noreply.github.com>
2024-07-30 12:07:48 +0700

    Add files via upload

--- frontend/src/features/firebase.js
+++ frontend/src/features/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database"; // Import getDatabase from firebase/database
 
 const firebaseConfig = {
-        apiKey: "AIzaSyB-C5LiItlozMBNSKhGKA-XXMtPwMwEj9A",
-        authDomain: "standby-c50ba.firebaseapp.com",
-        databaseURL: "https://standby-c50ba-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "standby-c50ba",
-        storageBucket: "standby-c50ba.appspot.com",
-        messagingSenderId: "627233552935",
-        appId: "1:627233552935:web:a89e271cbd1e22ec3bac02"
+    apiKey: "AIzaSyB-C5LiItlozMBNSKhGKA-XXMtPwMwEj9A",
+    authDomain: "standby-c50ba.firebaseapp.com",
+    databaseURL: "https://standby-c50ba-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "standby-c50ba",
+    storageBucket: "standby-c50ba.appspot.com",
+    messagingSenderId: "627233552935",
+    appId: "1:627233552935:web:a89e271cbd1e22ec3bac02"
 };
 
 const app = initializeApp(firebaseConfig);
