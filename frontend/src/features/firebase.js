26aef4db42ba1a816664d2eb0ba8a9359639370f
Sadam1122 <90631818+Sadam1122@users.noreply.github.com>
2024-07-30 11:45:00 +0700

    Update Sadam

--- frontend/src/features/firebase.js
+++ frontend/src/features/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database"; // Import getDatabase from firebase/database
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAUXfAdtk7JYrWkGRT9vhKUpoRrjzqkT6s",
-    authDomain: "aturbabyincubator-f5b13.firebaseapp.com",
-    databaseURL: "https://aturbabyincubator-f5b13-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "aturbabyincubator-f5b13",
-    storageBucket: "aturbabyincubator-f5b13.appspot.com",
-    messagingSenderId: "812262145388",
-    appId: "1:812262145388:web:d0ad6d846d6e270ba0c749"
+        apiKey: "AIzaSyB-C5LiItlozMBNSKhGKA-XXMtPwMwEj9A",
+        authDomain: "standby-c50ba.firebaseapp.com",
+        databaseURL: "https://standby-c50ba-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "standby-c50ba",
+        storageBucket: "standby-c50ba.appspot.com",
+        messagingSenderId: "627233552935",
+        appId: "1:627233552935:web:a89e271cbd1e22ec3bac02"
 };
 
 const app = initializeApp(firebaseConfig);
